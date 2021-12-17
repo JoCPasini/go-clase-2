09f42b2c39f6b96ecf119224fe35ac4e1e206422
Jose Pasini <jose.pasini@mercadolibre.com>
2021-12-17 11:56:21 -0300

    Primer commit

---
+++ ejercicios/ejercicio1.go
@@ -0,0 +1,16 @@
+package ejercicios
+
+//import "fmt"
+
+func Ejercicio1() {
+	/*Una empresa de chocolates necesita calcular el impuesto de sus empleados
+	al momento de depositar el sueldo, para cumplir el objetivo es necesario
+	crear una función que devuelva el impuesto de un salario.
+	Teniendo en cuenta que si la persona gana más de $50.000 se le descontará
+	un 17% del sueldo y si gana más de $150.000 se le descontará además un 10%
+	*/
+
+	//sueldo := 0.0
+	//fmt.Print("Ingrese su sueldo: $")
+	//fmt.Scanln(&sueldo)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JosePasiniMercadolibre/go-clase-2
+
+go 1.17
